3eb1c4c607d316b78bf8fa8a9c55c3fe55ad6ef9
King Grey <kinggrey9959@gmail.com>
2024-01-14 18:16:05 +0530

    Popular Jobs Section is done

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useRouter } from 'expo-router';
+import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants';
+import useFetch from '../../../hook/useFetch';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import styles from './popularjobs.style';
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch('search', {
+    query: 'React developer',
+    page: 1,
+    num_pages: 1,
+  });
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity style={styles}>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard item={item} handleCardPress={() => {}} />
+            )}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
---
+++ hook/useFetch.jsx
@@ -0,0 +1,49 @@
+import { RAPID_API_KEY } from '@env';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const rapidapi = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': rapidapi,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
