3c50857261b23a65249b0eca488615766ee0346f
King Grey <kinggrey9959@gmail.com>
2024-01-15 15:13:55 +0530

    Added General outline of Job-details Page

---
+++ app/job-details/[id].jsx
@@ -0,0 +1,84 @@
+import { Stack, useGlobalSearchParams, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import {
+  ActivityIndicator,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
+import { Company, JobTabs, ScreenHeaderBtn } from '../../components';
+import { COLORS, SIZES, icons } from '../../constants';
+import useFetch from '../../hook/useFetch';
+
+const JobDetails = () => {
+  const router = useRouter();
+  const params = useGlobalSearchParams();
+
+  const { data, isLoading, error, refetch } = useFetch('job-details', {
+    job_id: params.id,
+  });
+
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {};
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerTitleAlign: 'center',
+          headerTitle: '',
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension="60%"
+              handlePress={() => {}}
+            />
+          ),
+        }}
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size={'large'} color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went Wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No Data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { Text, View } from 'react-native';
 
-import styles from './company.style'
+// import styles from './company.style'
 
 const Company = () => {
   return (
     <View>
       <Text>Company</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
